7611c73c1ef614f563a215d14e20e808ca90ecd1
shixiaohan <xiaohan.shi@live.com>
2016-03-07 22:36:08 +0100

    modify KernelRidge

--- KernelRidge.py
+++ KernelRidge.py
@@ -11,17 +11,17 @@ class linear_kernel:
     	"""
     	create the kernel matrix
     	"""
-            self._x = x
-            self._Kmatrix = x.dot(x.T)
-            return self._Kmatrix
+        self._x = x
+        self._Kmatrix = x.dot(x.T)
+        return self._Kmatrix
     
     def Kernel_arr(self,x_arr):
-    	"""
-    	For prediction: 
-    	input an obsevation x
-    	output a n dimension vector K(x_i,x), i = 1,2,...,n
-    	"""
-            return np.asarray([self._x[i].dot(x_arr) for i in range(self._x.shape[0])])
+		"""
+		For prediction: 
+		input an obsevation x
+		output a n dimension vector K(x_i,x), i = 1,2,...,n
+		"""
+		return np.asarray([self._x[i].dot(x_arr) for i in range(self._x.shape[0])])
 
 
 
@@ -41,10 +41,10 @@ class rbf_kernel:
 	def kernel_create(self, x):
 		
 		"""
-    		create the kernel matrix
-    		input : X_train, a np.array on shape (n,p)
-    		"""
-
+		create the kernel matrix
+		input : X_train, a np.array on shape (n,p)
+		"""
+		x = np.asarray(x)
 		n = x.shape[0]
 		self._x = x
 
@@ -61,11 +61,11 @@ class rbf_kernel:
 	def Kernel_arr(self,x_arr):
 		
 		"""
-    		For prediction: 
-    		input  : an obsevation x, a np.array en shape (p,)
-    		output : a n dimension vector K(x_i,x), i = 1,2,...,n
-    		"""
-		
+		For prediction: 
+		input  : an obsevation x, a np.array en shape (p,)
+		output : a n dimension vector K(x_i,x), i = 1,2,...,n
+		"""
+
 		def k(x,y):
 			return np.exp(-1*(x-y).dot((x-y).T)/(2*self._sigma))
 
@@ -77,70 +77,72 @@ class rbf_kernel:
 
 class KernelRidge:
 
-    def __init__(self, lmb = 0.01, kernel = None):
-    	
-	"""
-	:Parameters:
-	lmb : float
-		The tuning parametre of ridge penalty 
-	kernel : string
-		Kernel type, can be 'linear' or 'rbf'
-	"""
-	
-    	if kernel == None:
-        	self._kernel = linear_kernel()
+    def __init__(self, lmb = 0.01, kernel = None, sigma = 1.0):
+	    	
+		"""
+		:Parameters:
+		lmb : float
+			The tuning parametre of ridge penalty 
+		kernel : string
+			Kernel type, can be 'linear' or 'rbf'
+		sigma : the main parameter of gaussien kernel, only used when kernel = 'rbf'
+		"""
 
-        if kernel == 'linear':
-        	self._kernel = linear_kernel()
+		if kernel == None:
+			self._kernel = linear_kernel()
 
-        if kernel == 'rbf':
-        	self._kernel = rbf_kernel()
-        	
-        if lmb < 0:
-            raise ValueError("lmb must be >= 0")
+		if kernel == 'linear':
+			self._kernel = linear_kernel()
 
-        self._lmb = float(lmb)        
-        self._alpha = None
+		if kernel == 'rbf':
+			self._kernel = rbf_kernel(sigma = sigma)
+			
+		if lmb < 0:
+		    raise ValueError("lmb must be >= 0")
+
+		self._lmb = float(lmb)        
+		self._alpha = None
 
   
                                 
     def fit(self, X, y):
     	
-	"""
-	To fit the model:
-	input:  X_train, a np.array on shape (n,p)
-		y_traiin, a np.array on shape (n,)
-	"""
-	
-        K_arr = self._kernel.kernel_create(X)
-        y_arr = np.asarray(y, dtype=np.float)
-        
-        n = K_arr.shape[0]
+		"""
+		To fit the model:
+		input:  X_train, a np.array on shape (n,p)
+			y_traiin, a np.array on shape (n,)
+		"""
 
-        self._alpha = np.linalg.solve(K_arr+self._lmb*n*np.eye(n),y_arr)
-                
+		K_arr = self._kernel.kernel_create(np.asarray(X))
+		y_arr = np.asarray(y, dtype=np.float)
+
+		n = K_arr.shape[0]
+
+		self._alpha = np.linalg.solve(K_arr+self._lmb*n*np.eye(n),y_arr)
+	                
     def predict(self, Xtest):
     	
-    	"""
-	To fit the model:
-	input: X_test, a np.array on shape (n,p)
-	output: y_train, a np.array on shape (n,)
-	"""
-	
-        p = [np.dot(self._alpha, self._kernel.Kernel_arr(Xtest[i])) for i in range(Xtest.shape[0])]
-        return np.asarray(p)
+		"""
+		To fit the model:
+		input: X_test, a np.array on shape (n,p)
+		output: y_train, a np.array on shape (n,)
+		"""
+		Xtest = np.asarray(Xtest)
+		p = [np.dot(self._alpha, self._kernel.Kernel_arr(Xtest[i])) for i in range(Xtest.shape[0])]
+		return np.asarray(p)
     
     def score(self,Xtest,Ytest):
     	
-    	"""
-	To fit the model:
-	input:  X_test, a np.array on shape (m,p)
-		y_train, a np.array on shape (m,)
-	"""
-    
-    	p = [np.dot(self._alpha, self._kernel.Kernel_arr(Xtest[i])) for i in range(Xtest.shape[0])]
-        p = np.asarray([round(i) for i in  p])
-        return float(sum(p == Ytest))/Xtest.shape[0]
+		"""
+		To fit the model:
+		input:  X_test, a np.array on shape (m,p)
+			y_train, a np.array on shape (m,)
+		"""
+		Xtest = np.asarray(Xtest)
+		Ytest = np.asarray(Ytest)
+		p = [np.dot(self._alpha, self._kernel.Kernel_arr(Xtest[i])) for i in range(Xtest.shape[0])]
+		p = np.asarray([round(i) for i in  p])
+		return float(sum(p == Ytest))/Xtest.shape[0]
 
 	
 
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from load_data import load_data, plot, flatten
 from combined_classifier import combined_classifier
 from sklearn.cross_validation import train_test_split
 from sklearn import svm
+from KernelRidge import *
 
 xtr, ytr = load_data()
 xtr = flatten(xtr)
 
-clf = svm.SVC(kernel='linear', C=1)
+
+clf = KernelRidge()
 
 clf_combined = combined_classifier(clf)
 x_train, x_test, y_train, y_test = train_test_split(xtr, ytr, test_size=0.2)
