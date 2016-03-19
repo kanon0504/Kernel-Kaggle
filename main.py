fc3c88d33f9226490118ef94cd97930f303a4872
Daheihei <yy42606r@gmail.com>
2016-03-19 11:17:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from combined_classifier import combined_classifier
 from sklearn.cross_validation import train_test_split
 from sklearn import svm
 from KernelRidge import *
-from kNN import kNN
+from kNN import kNN, verify
+from cross_val_score_kNN import cross_val_score_kNN
 
 xtr, ytr = load_data()
 xte = load_data_test()
@@ -44,39 +45,36 @@ clf_combined = combined_classifier(clf)
 wtf = cross_val_score(clf_combined,xtr,ytr)
 print wtf
 
-
-
-
-
-
-
-### parameter tuning (gaussian kernel + knn)
-#xtr1, xtr2, ytr1, ytr2 = train_test_split(xtr, ytr, test_size=0.2)
-#find the best k and sigma with xtr1 and ytr1
-#for i in [1,3,5,7,9,11,13]:
-#	for j in np.linspace(0.8, 1.2, num = 5).tolist():
-#		x_train, x_test, y_train, y_test = train_test_split(xtr1, ytr1, test_size=0.2)
-#		ypredict = kNN(i, x_train, y_train, x_test, j)
-#		score = 0.
-#		for q in range(len(y_test)):
-#			if ypredict[q] == y_test[q]:
-#				score += 1.
-#		print 'k:',i
-#		print 'sigma:',j
-#		print score/len(y_test)
-
-
-
 ### final predict
 #clf = KernelRidge(lmb=0.7, kernel = 'rbf', sigma=1)
 #clf_combined = combined_classifier(clf)
 #clf_combined.fit(xtr, ytr)
 #ypredict = clf_combined.predict(xte)
 
-#f = open('Yte.csv','w')
-#f.write("Id,Prediction"+"\n")
-#for i in enumerate(ypredict):
-#	f.write(str(i[0]+1)+','+str(i[1])+"\n")
-#f.close()
+
+
+### parameters tuning (gaussian kernel + knn)
+#find the best k and sigma with xtr and ytr by cross validation
+xtr1, xtr2, ytr1, ytr2 = train_test_split(xtr, ytr, test_size=0.2)
+for i in [1,3,5,7,9,11,13]:
+	for j in np.linspace(1, 1.7, num = 8).tolist():
+		scores = cross_val_score_kNN(xtr1, ytr1, i, j, 3) # cross validation with 3 folds
+		print 'k:',i
+		print 'sigma:',j
+		print scores
+### try model on xtr2 and ytr2 
+ypredict = kNN(1, xtr1, ytr1, xtr2, 1.6)
+scores = verify(ypredict, ytr2)
+print scores
+
+ypredict = kNN(5, xtr, ytr, xte,1.7)
+
+
+
+f = open('Yte.csv','w')
+f.write("Id,Prediction"+"\n")
+for i in enumerate(ypredict):
+	f.write(str(i[0]+1)+','+str(i[1])+"\n")
+f.close()
 
 
